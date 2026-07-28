35f1bb0e280712e44bc1bd2c6c26d6fd0bced16d
aayushma001 <aayushma_24152351@sunway.edu.np>
2026-07-28 13:53:50 +0545

    py

--- Fastapi_Authentication&Authorization/auth/main.py
+++ Fastapi_Authentication&Authorization/auth/main.py
@@ -4,6 +4,6 @@ import models, schemas, utils
 from auth_database import get_db
 
 
-SECRET_KEY = ""
+SECRET_KEY = "t57t1AR5VSQyDeq5f2n6j8JeIrAKvQ-cBO4Xu_eJ2vo"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
