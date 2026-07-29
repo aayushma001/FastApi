44a95d0f120af69a2d6117ffb45ab48046db7469
aayushma001 <aayushma_24152351@sunway.edu.np>
2026-07-29 11:27:40 +0545

    samplepage

--- Fastapi_Authentication&Authorization/auth/main.py
+++ Fastapi_Authentication&Authorization/auth/main.py
@@ -2,8 +2,15 @@ from fastapi import FastAPI, Depends, HTTPException, status
 from sqlalchemy.orm import Session
 import models, schemas, utils
 from auth_database import get_db
+from jose import jwt
+from datetime import datetime, timedelta
 
 
 SECRET_KEY = "t57t1AR5VSQyDeq5f2n6j8JeIrAKvQ-cBO4Xu_eJ2vo"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+#helper function that takes user data
+def create_access_token(data:dict):
+    to_encode = data.copy()
+    expire = date.utcnow()
