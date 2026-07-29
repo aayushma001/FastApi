6aaa7a477b947bd6ec23a45fa424352d2bd40a82
aayushma001 <aayushma_24152351@sunway.edu.np>
2026-07-29 11:34:49 +0545

    authentication

--- Fastapi_sample/main.py
+++ Fastapi_sample/main.py
@@ -1,8 +1,19 @@
 from fastapi import Depends, FastAPI, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from pydantic import BaseModel
+from datetime import datetime, timedelta
 
-app = FastAPI()
+from jose import JWTError, jwt 
+from passlib.context import CryptoContext
 
-@app.get("/test/{item_id}/")
-async def test(item_id: str):
-    return {"Hello": item_id}
+# app = FastAPI()
+# class Data(BaseModel):
+#     name:str
+
+# @app.post("/create/")
+# async def create(data: Data):
+#     return {"data": data}
+
+# @app.get("/test/{item_id}/")
+# async def test(item_id: str, query: int = 1):
+#     return {"Hello": item_id}
