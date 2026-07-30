bda7e394ada7d627586714e4657ec978cde98b44
aayushma001 <aayushma_24152351@sunway.edu.np>
2026-07-30 11:37:06 +0545

    py1

--- Fastapi_sample/main.py
+++ Fastapi_sample/main.py
@@ -35,7 +35,7 @@ class UserInDB(User):
     hashed_password: str
 
 pwd_context = CryptoContext(schemas=["bycrypt"], deprecated="auto")
-oauth_2_scheme = OAuth2PasswordBearer(tokenUrl = "token")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl = "token")
 app = FastAPI()
 
 def verify_password(plain_password, hashed_passwords):
@@ -61,6 +61,26 @@ def create_access_token(data: dict, expires_delta: timedelta or None = None):
         expire = datetime.utcnow() + expires_delta
     else:
         expire = datetime.utcnow() + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+async def get_current_user(token: str = Depends(oauth2_scheme)):
+    credential_exception = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                                         Ddetail="could not validate credentials", headers = {"WWW.Aauthenticate": "Bearer"})
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithm=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credential_exception
+        token_data = TokenData(username=username)
+    except JWTError:
+        raise credential_exception
+    user = get_user(db, username=token_data.username)
+    if user is None:
+        raise credential_exception
+    return user
+
 
 
 
