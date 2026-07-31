ae30275700c0046d33bd4ca647ecaeea59927ada
aayushma001 <aayushma_24152351@sunway.edu.np>
2026-07-31 11:43:25 +0545

    py1

--- Fastapi_sample/main.py
+++ Fastapi_sample/main.py
@@ -3,18 +3,18 @@ from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from pydantic import BaseModel
 from datetime import datetime, timedelta
 from jose import JWTError, jwt 
-from passlib.context import CryptoContext
+from passlib.context import CryptContext
 
-SECRET_KEY = ""
+SECRET_KEY = "83daa0256a2289b0fb23693bf1f6034d44396675749244721a2b20e896e11662"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
-fake_db = {
+db = {
     "tim": {
-        "username": "tim",
+        "   username": "tim",
         "full_name": "TimRusscia",
         "email": "tim@gmail.com",
-        "hashed_password": "",
+        "hashed_password": "$2b$12$M9gFgEnfuZONJ7I46RcSGONfjMPZCBVbU5k3EY39FaYHjdwuPpp6q",
         "disabled": False
     }
 }
@@ -34,11 +34,11 @@ class User(BaseModel):
 class UserInDB(User):
     hashed_password: str
 
-pwd_context = CryptoContext(schemas=["bycrypt"], deprecated="auto")
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl = "token")
 app = FastAPI()
 
-def verify_password(plain_password, hashed_passwords):
+def verify_password(plain_password, hashed_password):
     return pwd_context.verify(plain_password, hashed_password)
 
 def get_password_hash(password):
@@ -80,13 +80,31 @@ async def get_current_user(token: str = Depends(oauth2_scheme)):
     if user is None:
         raise credential_exception
     return user
+async def get_current_active_user(current_user: UserInDB = Depends(get_current_user)):
+    if current_user.disabled:
+        raise HTTPException(status_code=400, detail="Inactive user")
+    return current_user
+@app.post("/token", response_model=Token)
+async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends()):
+    user = authenticate_user(db, form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                            detail="Incorrect username or password", headers={"WWW-Authenticate": "Bearer"})
+    access_token_expires = timedelta(minutes = ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": user.username}, expires_delta=access_token_expires)
+    return {"access_token": access_token, "token_type": "bearer"}
+
+@app.get("/users/me/", response_model=User)
+async def read_users_me(current_user: User = Depends(get_current_active_user)):
+    return current_user
+@app.get("users/me/items")
+async  def read_own_items(current_user: User = Depends(get_current_active_user)):
+    return [{"item_id": 1, "owner": current_user}]
 
 
-
-
-
-
-
+# pwd = get_password_hash("tim1234")
+# print(pwd)
 
 
 
