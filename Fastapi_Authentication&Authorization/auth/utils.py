2892507bf5d77d5b11eb5ecb5b7500e92b52b449
aayushma001 <aayushma_24152351@sunway.edu.np>
2026-08-06 22:24:59 +0545

    revise

--- Fastapi_Authentication&Authorization/auth/utils.py
+++ Fastapi_Authentication&Authorization/auth/utils.py
@@ -6,3 +6,13 @@ def hash_password(password:str) -> str:
     return pwd_context.hash(password)
 def verify_password(plain_password:str, hashed_password: str) -> bool:
     return pwd_context.verify(plain_password, hash_password)
+def hash_password(password:str) -> str:
+    return pwd_context.hash(password)
+def verify_password(plain_password:str, hashed_password: str) -> bool:
+    return pwd_context.verify(plain_password, hash_password)def hash_password(password:str) -> str:
+    return pwd_context.hash(password)
+def verify_password(plain_password:str, hashed_password: str) -> bool:
+    return pwd_context.verify(plain_password, hash_password)def hash_password(password:str) -> str:
+    return pwd_context.hash(password)
+def verify_password(plain_password:str, hashed_password: str) -> bool:
+    return pwd_context.verify(plain_password, hash_password)
